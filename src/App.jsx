12fbc5ad68657dfe9ec246dce7d6f761dc2dfd5c
Sjpandya0210 <Sjpandya1991@gmail.com>
2024-01-08 17:54:38 -0500

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
+import ContactRow from './components/ContactRow'
+import SelectedContact from './components/SelectedContact'
+
 
 function App() {
- 
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-
+      {selectedContactId ? (
+        <SelectedContact> Selected Contact View </SelectedContact>
+      ) : (
+        <ContactList />
+      )}
     </>
   )
 }
